4906e3691b63ba158ed13c0380b4f59a1f1c1dfd
Vaishnavi Shrivastava <shrivastavavaishnavi77@gmail.com>
2026-02-12 15:26:30 +0530

    add Lec6

---
+++ Lec6/index.js
@@ -0,0 +1,21 @@
+const express = require("express")
+const app = express()
+
+
+app.get("/about/:id/:user", (req, res) => {
+  res.send("Hello Miss Vaishnavi")
+})
+
+// app.use("/contact",(req,res)=>{
+//     res.send("I am your contact page")
+// })
+// app.use("/detail",(req,res)=>{
+//     res.send("I am your detail page")
+// })
+// app.use("/",(req,res)=>{
+//     res.send("I am Home page")
+// })
+app.listen(4000,()=>{
+    console.log("Im Listening at port 4000");
+    
+})
